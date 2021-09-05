fe5b5991c3711bde89901d8e7990fa657944c6ec
nipunjain <nipunjain119@gmail.com>
2021-09-06 01:48:52 +0530

    day11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
           ),
           
         ),
-        drawer: myDrawer(),
+        drawer: MyDrawer(),
     );
   }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,21 +1,70 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/utils/routes.dart';
 
-
-class myDrawer extends StatelessWidget {
-const myDrawer({Key? key}) : super(key: key);
-
-@override
-Widget build(BuildContext context) {
-  return Drawer(
-    child: ListView(
-      children: [
-        DrawerHeader(child: UserAccountsDrawerHeader(accountName: Text("pawan"), accountEmail: Text(:mtechviral),))
-        Padding : EdgeInsets.zero,;
-        child : UserAccountsDrawerHeader(
-          margin : Edge
-        )]
-      ],
-    ),
-  );
-}
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = "https://www.garnudsalg.dk/images/kategoribilleder/Vignetter/barn.jpg";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("Nipun Jain"),
+                accountEmail: Text("nipunjain119@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            Divider(color: Colors.white,thickness: 1,),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor:1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "profile",
+                textScaleFactor:1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor:1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
