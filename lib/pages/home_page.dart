8c9e6c1a053493e5e9979060c453d6c32b91804e
nipunjain <nipunjain119@gmail.com>
2021-09-13 16:56:41 +0530

    day 16

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/Item_widget.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -40,68 +41,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
+    return SafeArea(
+      child: Container(
+        padding: Vx.m32,
+        child: Scaffold(
+          body: Column(
+            children: [
+              "Catalog App".text.xl5.bold.color(MyTheme.darkBluish).make(),
+            ],
+          )
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ?
-            //   ListView.builder(
-            //   itemCount: CatalogModel.items.length,
-            //   itemBuilder: (context, index) {
-            //     return ItemWidget(
-            //       item: CatalogModel.items[index],
-            //     );
-            //   },
-            // )
-            GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16),
-                    
-                itemBuilder: (context, index) {
-                  final item = CatalogModel.items[index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    elevation: 5.0,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                    child: GridTile(
-                      child: Image.network(item.image),
-                      header: Container(
-                        child: Text(item.name,style: TextStyle(color: Colors.white),),
-
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple
-                        ),
-                      ),
-                      footer:Container(
-                        child: Text(item.price.toString(),style: TextStyle(color: Colors.white),),
-
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.black
-                        ),
-                      ), 
-                    ),
-                  );
-                },
-                itemCount: CatalogModel.items.length,
-              )
-            : Center(
-                child: LinearProgressIndicator(),
-              ),
-      ),
-      // body: Center(
-      //   child: Container(
-      //     child: Text("welcome to $days of flutter by $name"),
-      //   ),
-
-      // ),
-      drawer: MyDrawer(),
     );
   }
 }
