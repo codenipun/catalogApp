4fc529b986527dda04f938af91efe15b71c0168f
nipunjain <nipunjain119@gmail.com>
2021-09-20 17:40:58 +0530

    200921

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       darkTheme: MyTheme.darkTheme(context),
       routes: {
-        "/": (context) => LoginPage(),
+        "/": (context) => HomePage(),
         MyRoutes.homeRoute : (context) => HomePage(),
         MyRoutes.loginRoute : (context) => LoginPage(),
         MyRoutes.cartRoute : (context) => CartPage(),
